6e99917cff9c188abcd8fd69f0de0e118c71ce2e
Sonya Sheblova <sonyach.1110@gmail.com>
2022-03-30 02:06:32 +0300

    11

---
+++ module_11/1.py
@@ -0,0 +1,50 @@
+import unittest
+a = [4, 5, 6, 10, 1, 3]
+
+
+def merge_sort(a):
+    if len(a) < 2:
+        return a[:]
+    else:
+        median = int(len(a) / 2)
+        left = merge_sort(a[:median])
+        right = merge_sort(a[median:])
+        return merge(left, right)
+
+
+def merge(left, right):
+    res = []
+    i, j = 0, 0
+    while i < len(left) and j < len(right):
+        if left[i] < right[j]:
+            res.append(left[i])
+            i += 1
+        else:
+            res.append(right[j])
+            j += 1
+    while i < len(left):
+        res.append(left[i])
+        i += 1
+    while j < len(right):
+        res.append(right[j])
+        j += 1
+    return res
+
+
+print(merge_sort(a))
+
+
+class TestSort(unittest.TestCase):
+
+    def test1(self):
+        c = [6, 8, 3, 1]
+        s = sorted(c)
+        self.assertEqual(merge_sort(c), s)
+
+    def test2(self):
+        c = [-6, 0, 0, 1]
+        s = sorted(c)
+        self.assertEqual(merge_sort(c), s)
+
+
+unittest.main()
